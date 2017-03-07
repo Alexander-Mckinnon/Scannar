cf864a5345a908a55e03a207c601e5d52faf9434
Alex <bartholomew_mckinnon@yahoo.com>
2017-03-06 21:54:52 -0800

    Timed imaged slider
    added a timed image slider to the webpage

--- scripts/script.js
+++ scripts/script.js
@@ -4,41 +4,28 @@
    $(".img-container img").hide();
    $(".img-container img").eq(currentPic).show();
     numOfPics = $(".img-container img").length;
-    $(".next").on("click", function(){
-     
-         if(currentPic < numOfPics){
-             $(".img-container img").eq(currentPic).hide();
+    function nextImage(){
+        if(currentPic < numOfPics){
+             $(".img-container img").eq(currentPic)
+                 .hide();
              currentPic = currentPic+1;
-             $(".img-container img").eq(currentPic).fadeIn().show();
+        
+             $(".img-container img").eq(currentPic)
+                 .fadeIn( )
+                 .show();
               
              
              if(currentPic == numOfPics){
              currentPic = 0;
-             $(".img-container img").eq(currentPic).fadeIn().show();
+             $(".img-container img").eq(currentPic).fadeIn( ).show( ).end();
+                 
          }// nested if
         
 
          }// end if
-         
-          
-    });//end event listener
-    
-    
-    $(".prev").on("click", function(){
-        if(currentPic < numOfPics){
-            $(".img-container img").eq(currentPic).hide();
-             currentPic = currentPic-1;
-             $(".img-container img").eq(currentPic).fadeIn().show();
-              
-            
-            if(currentPic == -1){
-             currentPic = numOfPics -1;
-            $(".img-container img").eq(currentPic).show();
-            
-            
-        } //end nested if
         
-        }// end if
-            
-    });
+    }//end function
+     
+     setInterval(nextImage, 7000);
 });//end main
+
